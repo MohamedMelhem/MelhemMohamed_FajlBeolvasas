8cb14e2229a72c5f2bb2cecce69bedf7ebe61e26
Petrik Diak <diak@petrik.hu>
2025-02-14 12:04:19 +0100

    Első 5 Feladat Kész.

---
+++ FajlBeolvasas/Karakter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FajlBeolvasas
+{
+	internal class Karakter
+	{
+		private string nev;
+		private int ero;
+		private int eletero;
+		private int szint;
+
+		public Karakter(string nev, int ero, int eletero, int szint)
+		{
+			this.nev = nev;
+			this.ero = ero;
+			this.eletero = eletero;
+			this.szint = szint;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Ero { get => ero; set => ero = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Szint { get => szint; set => szint = value; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+		public override string? ToString()
+		{
+			return $"Név: {nev} Szint :  {Szint}  Élet : {eletero} Ero : {ero}";
+		}
+	}
+}
--- FajlBeolvasas/Program.cs
+++ FajlBeolvasas/Program.cs
@@ -1,27 +1,99 @@
-﻿namespace FajlBeolvasas
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+
+namespace FajlBeolvasas
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Karakter> karakterek = [];
 
-
-
-
-
-
-
-
-
-
-
-
+			Beolvasas("karakterek.txt", karakterek);
+			Eletlegtobb(karakterek);
+			AtlagSzint(karakterek);
+			RendezoErosseg(karakterek);
+			Nagyobbe50nel(karakterek);
 
 
 
 
 
 		}
-	}
+
+		static void Beolvasas(string fajlnev, List<Karakter> karakterek)
+		{
+			StreamReader sr = new(fajlnev);
+			string[] sorok = File.ReadAllLines(fajlnev);
+
+
+
+			sr.ReadLine();
+
+
+
+
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(';');
+
+				Karakter karakter = new(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
+		}
+
+		static void Eletlegtobb(List<Karakter> karakterek)
+		{
+
+
+
+			int max = 0;
+			Karakter legtobbEletero = null;
+			foreach (var item in karakterek)
+			{
+				if (item.Eletero > max)
+				{
+					max = item.Eletero;
+					legtobbEletero = item;
+				}
+			}
+			Console.WriteLine("Legtöbb életű karakter:");
+			Console.WriteLine($"Legtöbb élet: {legtobbEletero}");
+
+		}
+		static void AtlagSzint(List<Karakter> karakterek)
+		{
+			int osszeg = 0;
+			foreach (var item in karakterek)
+			{
+				osszeg += item.Szint;
+			}
+			double atlag = (double)osszeg / karakterek.Count;
+			Console.WriteLine($"Átlag szint: {atlag}");
+		}
+		static void RendezoErosseg(List<Karakter> karakterek)
+		{
+			karakterek.Sort((x, y) => x.Ero.CompareTo(y.Ero));
+			Console.WriteLine("Erő szerint rendezve:");
+			foreach (var item in karakterek)
+			{
+				Console.WriteLine(item);
+			}
+		}
+		static void Nagyobbe50nel(List<Karakter> karakterek)
+		{
+			Console.WriteLine("50-nél nagyobb életű karakterek:");
+			foreach (var item in karakterek)
+			{
+				if (item.Eletero > 50)
+				{
+					Console.WriteLine(item);
+				}
+			}
+		}
+
+
+
+	} 
 }
